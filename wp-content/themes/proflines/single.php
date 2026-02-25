44b6ae85c6b22438d4a1dd8aa9c6d05c9057117b
Evgeniy Zhuranskiy <evgenijzuranskij72@gmail.com>
2026-02-25 12:29:51 +0200

    auto-commit

--- wp-content/themes/proflines/single.php
+++ wp-content/themes/proflines/single.php
@@ -4,41 +4,46 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $categories = get_the_category();
+
 if (!empty($categories)) {
-    $category_slug = $categories[0]->slug;
-        
-    switch ($category_slug) {
+    $slugs = wp_list_pluck($categories, 'slug');
 
-        case 'marketingovy-prieskum-services':
-            get_template_part('single-marketingovy-prieskum-services');
-            exit;
-            
-        case 'odblokovanie-google-merchant-cente':
-            get_template_part('single-odblokovanie-google-merchant-cente');
-            exit;
-            
-        case 'ppc':
-            get_template_part('single-ppc');
-            exit;
-            
-        case 'seo-audit':
-            get_template_part('single-seo-audit');
-            exit;
-            
-        case 'ina-sluzba':
-            if (strpos($_SERVER['REQUEST_URI'], '/brif/') !== false) {
-                get_template_part('single-services_brif');
-                exit;
-            }
-            wp_redirect(get_permalink() . 'brif/');
-            exit;
-            
-        case 'blog':
-            get_template_part('single-blog');
-            exit;
+    if (in_array('tvorba-webu', $slugs)) {
+        get_template_part('single-web');
+        exit;
+    }
 
-        case 'web':
-            get_template_part('single-web');
+    if (in_array('ppc', $slugs)) {
+        get_template_part('single-ppc');
+        exit;
+    }
+
+    if (in_array('seo-audit', $slugs)) {
+        get_template_part('single-seo-audit');
+        exit;
+    }
+
+    if (in_array('marketingovy-prieskum-services', $slugs)) {
+        get_template_part('single-marketingovy-prieskum-services');
+        exit;
+    }
+
+    if (in_array('odblokovanie-google-merchant-cente', $slugs)) {
+        get_template_part('single-odblokovanie-google-merchant-cente');
+        exit;
+    }
+
+    if (in_array('blog', $slugs)) {
+        get_template_part('single-blog');
+        exit;
+    }
+
+    if (in_array('ina-sluzba', $slugs)) {
+        if (strpos($_SERVER['REQUEST_URI'], '/brif/') !== false) {
+            get_template_part('single-services_brif');
             exit;
+        }
+        wp_redirect(get_permalink() . 'brif/');
+        exit;
     }
 }
