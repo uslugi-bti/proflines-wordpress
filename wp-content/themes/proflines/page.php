c249a0eaed3bfde5fa1ee531d5ac04d6cf4ee1c2
Evgeniy Zhuranskiy <evgenijzuranskij72@gmail.com>
2026-02-26 18:13:11 +0200

    auto-commit

---
+++ wp-content/themes/proflines/page.php
@@ -0,0 +1,99 @@
+<?php get_header(); ?>
+<main class="technical-page">
+    <div class="breadcrump">
+        <div class="container">
+            <ul class="breadcrump__body">
+                <li class="breadcrump__item">
+                    <a href>Domov</a>
+                </li>
+                <li class="breadcrump__item">
+                    <a href>Bal&#xED;ky slu&#x17E;ieb</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <section class="technical">
+        <div class="container">
+            <div class="technical__body">
+                <div class="technical__content">
+                    <div class="technical-content__head">
+                        <div class="technical-content__title">
+                            <h1>Z&#xE1;sady ochrany osobn&#xFD;ch &#xFA;dajov (GDPR)</h1>
+                        </div>
+                        <div class="technical-content__text">
+                            <p>aktualizovan&#xE9;/updated: 7-Nov-25</p>
+                        </div>
+                    </div>
+                    <div class="technical-content__body" id="gdpr">
+                        <h2>Z&#xE1;sady ochrany osobn&#xFD;ch &#xFA;dajov (GDPR)</h2>
+                        <p>Spolo&#x10D;nos&#x165; ProfLines s.r.o. (I&#x10C;O: 53398467, DI&#x10C;: SK2121411105, so s&#xED;dlom: Lermontovova 911/3, Bratislava, 81105, Slovensk&#xE1; republika) sprac&#xFA;va osobn&#xE9; &#xFA;daje v s&#xFA;lade s Nariaden&#xED;m Eur&#xF3;pskeho parlamentu a Rady (E&#xDA;) 2016/679 (GDPR) a z&#xE1;konom &#x10D;. 18/2018 Z.z.  o ochrane osobn&#xFD;ch &#xFA;dajov. Cie&#x13E;om t&#xFD;chto z&#xE1;sad je informova&#x165; v&#xE1;s o sp&#xF4;sobe, rozsahu a &#xFA;&#x10D;ele sprac&#xFA;vania osobn&#xFD;ch &#xFA;dajov.</p>
+                        <h3>1. Prev&#xE1;dzkovate&#x13E;</h3>
+                        <p>Spolo&#x10D;nos&#x165; ProfLines s.r.o. (I&#x10C;O: 53398467, DI&#x10C;: SK2121411105, so s&#xED;dlom: Lermontovova 911/3, Bratislava, 81105, Slovensk&#xE1; republika) sprac&#xFA;va osobn&#xE9; &#xFA;daje v s&#xFA;lade s Nariaden&#xED;m Eur&#xF3;pskeho parlamentu a Rady (E&#xDA;) 2016/679 (GDPR) a z&#xE1;konom &#x10D;. 18/2018 Z.z. o ochrane osobn&#xFD;ch &#xFA;dajov. Cie&#x13E;om t&#xFD;chto z&#xE1;sad je informova&#x165; v&#xE1;s o sp&#xF4;sobe, rozsahu a &#xFA;&#x10D;ele sprac&#xFA;vania osobn&#xFD;ch &#xFA;dajov.</p>
+                        <h3>2. &#xDA;&#x10D;el sprac&#xFA;vania &#xFA;dajov</h3>
+                        <p>Osobn&#xE9; &#xFA;daje s&#xFA; sprac&#xFA;van&#xE9; za &#xFA;&#x10D;elom:<br>- komunik&#xE1;cie s klientmi prostredn&#xED;ctvom e-mailu,<br>- spracovania objedn&#xE1;vok a zmluvn&#xFD;ch vz&#x165;ahov,<br>- marketingovej komunik&#xE1;cie a newsletterov (po udelen&#xED; s&#xFA;hlasu),<br>- analytiky webovej str&#xE1;nky pomocou n&#xE1;strojov Google Analytics, Meta Pixel a Microsoft Clarity.</p>
+                        <h3>3. Rozsah sprac&#xFA;van&#xFD;ch &#xFA;dajov</h3>
+                        <p>Sprac&#xFA;vame tieto &#xFA;daje: meno, priezvisko, e-mailov&#xE1; adresa, telef&#xF3;nne &#x10D;&#xED;slo, n&#xE1;zov spolo&#x10D;nosti, IP adresa, &#xFA;daje o pou&#x17E;&#xED;van&#xED; webovej str&#xE1;nky, preferencie a interakcie so slu&#x17E;bami ProfLines.</p>
+                        <h3>4. Pr&#xE1;vny z&#xE1;klad sprac&#xFA;vania</h3>
+                        <p>Osobn&#xE9; &#xFA;daje sprac&#xFA;vame na z&#xE1;klade:<br><br>- plnenia zmluvy (&#x10D;l. 6 ods. 1 p&#xED;sm. b GDPR),<br><br>- opr&#xE1;vnen&#xE9;ho z&#xE1;ujmu prev&#xE1;dzkovate&#x13E;a (&#x10D;l. 6 ods. 1 p&#xED;sm. f GDPR),<br><br>- s&#xFA;hlasu dotknutej osoby (&#x10D;l. 6 ods. 1 p&#xED;sm. a GDPR).</p>
+                        <h3>5. Uchov&#xE1;vanie &#xFA;dajov</h3>
+                        <p>Osobn&#xE9; &#xFA;daje uchov&#xE1;vame po dobu nevyhnutn&#xFA; na naplnenie &#xFA;&#x10D;elu, najviac v&#x161;ak 5 rokov od ukon&#x10D;enia spolupr&#xE1;ce, alebo do odvolania s&#xFA;hlasu dotknutej osoby.</p>
+                        <h3>6. Pr&#xE1;va dotknut&#xFD;ch os&#xF4;b</h3>
+                        <p>Dotknut&#xE9; osoby maj&#xFA; pr&#xE1;vo na pr&#xED;stup k svojim &#xFA;dajom, ich opravu, vymazanie, obmedzenie sprac&#xFA;vania, ako aj pr&#xE1;vo namieta&#x165; proti sprac&#xFA;vaniu a pr&#xE1;vo na prenosnos&#x165; &#xFA;dajov. &#x17D;iadosti o uplatnenie pr&#xE1;v je mo&#x17E;n&#xE9; zasla&#x165; na e-mail: gdpr@proflines.agency.</p>
+                        <h3>7. Zdie&#x13E;anie &#xFA;dajov</h3>
+                        <p>Osobn&#xE9; &#xFA;daje m&#xF4;&#x17E;u by&#x165; zdie&#x13E;an&#xE9; s d&#xF4;veryhodn&#xFD;mi partnermi, ktor&#xED; zabezpe&#x10D;uj&#xFA; technick&#xFA; podporu, analytick&#xE9; a marketingov&#xE9; slu&#x17E;by. V&#x161;etky subjekty s&#xFA; viazan&#xE9; povinnos&#x165;ou ml&#x10D;anlivosti a sprac&#xFA;vaj&#xFA; &#xFA;daje v s&#xFA;lade s GDPR.</p>
+                        <h3>8. Zabezpe&#x10D;enie &#xFA;dajov</h3>
+                        <p>ProfLines s.r.o. prij&#xED;ma primeran&#xE9; technick&#xE9; a organiza&#x10D;n&#xE9; opatrenia na ochranu osobn&#xFD;ch &#xFA;dajov pred stratou, zneu&#x17E;it&#xED;m alebo neopr&#xE1;vnen&#xFD;m pr&#xED;stupom.</p>
+                        <h3>9. Kontakt</h3>
+                        <p>V pr&#xED;pade ot&#xE1;zok alebo podnetov t&#xFD;kaj&#xFA;cich sa ochrany osobn&#xFD;ch &#xFA;dajov n&#xE1;s kontaktujte e-mailom na: gdpr@proflines.agency<br><br>ProfLines s.r.o., Bratislava</p>
+                    </div>
+                    <div class="technical-content__body" id="policy">
+                        <h2>Privacy Policy (GDPR)</h2>
+                        <p>ProfLines s.r.o. (Company ID: 53398467, VAT ID: SK2121411105, registered office: Lermontovova 911/3, Bratislava, 81105, Slovakia) processes personal data in accordance with Regulation (EU) 2016/679 (GDPR) and Act No. 18/2018 Coll. on the Protection of Personal Data. The purpose of this policy is to inform you about the manner, scope, and purpose of personal data processing.</p>
+                        <h3>1. Data Controller</h3>
+                        <p>The controller of personal data is ProfLines s.r.o., Lermontovova 911/3, Bratislava, 81105, Slovakia. Office Address: Legion&#xE1;rska 1/1, 831 04 Bratislava Contact email: info@proflines.agency</p>
+                        <h3>2. Purpose of Processing</h3>
+                        <p>Personal data is processed for the following purposes:<br>- communication with clients via email,<br>- order processing and contractual relationships,<br>- marketing communication and newsletters (upon consent),<br>- website analytics through Google Analytics, Meta Pixel, and Microsoft Clarity.</p>
+                        <h3>3. Scope of Processed Data</h3>
+                        <p>We process the following data: name, surname, email address, phone number, company name, IP address, website usage data, preferences, and interactions with ProfLines services.</p>
+                        <h3>4. Legal Basis for Processing</h3>
+                        <p>We process personal data based on:<br>- the performance of a contract (Art. 6(1)(b) GDPR),<br>- the legitimate interests of the controller (Art. 6(1)(f) GDPR),<br>- the consent of the data subject (Art. 6(1)(a) GDPR).</p>
+                        <h3>5. Data Retention</h3>
+                        <p>Personal data is retained for the period necessary to fulfill its purpose, but not longer than 5 years after the termination of cooperation, or until the consent is withdrawn by the data subject.</p>
+                        <h3>6. Rights of Data Subjects</h3>
+                        <p>Data subjects have the right to access, rectify, erase, restrict processing, object to processing, and the right to data portability. Requests to exercise these rights can be sent to: gdpr@proflines.agency.</p>
+                        <h3>7. Data Sharing</h3>
+                        <p>Personal data may be shared with trusted partners providing technical support, analytics, and marketing services. All third parties are bound by confidentiality and process data in compliance with the GDPR.</p>
+                        <h3>8. Data Security</h3>
+                        <p>ProfLines s.r.o. implements appropriate technical and organizational measures to protect personal data from loss, misuse, or unauthorized access.</p>
+                        <h3>9. Kontakt</h3>
+                        <p>If you have any questions or requests regarding data protection, please contact us at: gdpr@proflines.agency<br><br>ProfLines s.r.o., Bratislava</p>
+                    </div>
+                    <div class="technical-content__banner">
+                        <div class="technical-content-banner__title">
+                            <h2>Pr&#xE1;vny z&#xE1;klad sprac&#xFA;vania</h2>
+                        </div>
+                        <div class="technical-content-banner__button button">
+                            <a href>Z&#xED;ska&#x165; bezplatn&#xFA; konzult&#xE1;ciu</a>
+                        </div>
+                        <div class="technical-content-banner__button">
+                            <a href>Z&#xED;ska&#x165; bezplatn&#xFD; pl&#xE1;n</a>
+                        </div>
+
+                        <div class="technical-content-banner__logo">
+                        <picture><source srcset="img/logo/02.svg" type="image/webp"><img src="img/logo/02.svg" alt="logo"></picture>
+                    </div>
+                    </div>
+                </div>
+                <ul class="technical__list">
+                    <li class="technical-list__item">
+                        <a href="#gdpr">Z&#xE1;sady ochrany osobn&#xFD;ch &#xFA;dajov (GDPR)</a>
+                    </li>
+                    <li class="technical-list__item">
+                        <a href="#policy">Privacy Policy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
