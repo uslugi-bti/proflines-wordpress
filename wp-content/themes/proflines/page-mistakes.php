21c267dd22ddbd249921886ae1db85df5996a10d
Evgeniy Zhuranskiy <evgenijzuranskij72@gmail.com>
2026-02-26 14:25:27 +0200

    auto-commit

---
+++ wp-content/themes/proflines/page-mistakes.php
@@ -0,0 +1,119 @@
+<?php get_header(); ?>
+<main class="mistakes-page">
+    <div class="breadcrump">
+        <div class="container">
+            <ul class="breadcrump__body">
+                <li class="breadcrump__item">
+                    <a href>Domov</a>
+                </li>
+                <li class="breadcrump__item">
+                    <a href>Soci&#xE1;lna iniciat&#xED;va</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <section class="mistakes">
+        <div class="container">
+            <div class="single__body">
+                <div class="single__head">
+                    <div class="single-head__body">
+                        <div class="single-head__title">
+                            <h1>21 ch&#xFD;b pri &#x161;tarte podnikania na Slovensku</h1>
+                        </div>
+                        <div class="single-head__text">
+                            <p>Spustenie podnikania na Slovensku je vzru&#x161;uj&#xFA;ca cesta, ale aj zlo&#x17E;it&#xFD; proces pln&#xFD; n&#xE1;strah.</p>
+                        </div>
+                        <div class="single-head__banner">
+                            <p>&#x160;tatistiky ukazuj&#xFA;, &#x17E;e 10% a&#x17E; 25% startupov nepre&#x17E;ije prv&#xFD;ch 5 rokov.<br>Mnoh&#xFD;m z t&#xFD;chto zlyhan&#xED; sa d&#xE1; pred&#xED;s&#x165; poznan&#xED;m naj&#x10D;astej&#x161;&#xED;ch ch&#xFD;b a sp&#xF4;sobov, ako im zabr&#xE1;ni&#x165;. Tento kontroln&#xFD; zoznam pom&#xF4;&#x17E;e vyhn&#xFA;&#x165; sa kritick&#xFD;m chyb&#xE1;m, ktor&#xFD;ch sa podnikatelia dop&#xFA;&#x161;&#x165;aj&#xFA; pri registr&#xE1;cii a veden&#xED; podnikania na Slovensku.<br>Pou&#x17E;ite ho ako cestovn&#xFA; mapu pre &#xFA;spe&#x161;n&#xFD; &#x161;tart podniku.</p>
+                        </div>
+                    </div>
+                    <div class="single-head__img">
+                        <picture><source srcset="<?php bloginfo('template_url'); ?>/assets/img/mistakes/01.webp" type="image/webp"><img src="<?php bloginfo('template_url'); ?>/assets/img/mistakes/01.png" alt="single"></picture>
+                    </div>
+                </div>
+            </div>
+            <div class="technical__body">
+                <div class="technical__content">
+                    <div class="mistakes__body">
+                        <div class="mistakes__item" id="legal">
+                            <div class="mistakes-item__title">
+                                <h3>Pr&#xE1;vne a registra&#x10D;n&#xE9; chyby</h3>
+                            </div>
+                            <div class="mistakes-item__body">
+                                <div class="mistakes-item__item">
+                                    <div class="mistakes-item-item__tilte">
+                                        <h4>Nespr&#xE1;vna vo&#x13E;ba pr&#xE1;vnej formy</h4>
+                                    </div>
+                                    <div class="mistakes-item-item__body">
+                                        <div class="mistakes-item-item__column red">
+                                            <p><strong>Chyba:</strong></p>
+                                            <p>Vo&#x13E;ba medzi &#x17E;ivnos&#x165;ou a s.r.o. bez zoh&#x13E;adnenia &#x161;pecif&#xED;k podnikania.</p>
+                                        </div>
+                                        <div class="mistakes-item-item__column blue">
+                                            <p><strong>Rie&#x161;enie:</strong></p>
+                                            <p>Konzult&#xE1;cia s pr&#xE1;vnikom na v&#xFD;ber optim&#xE1;lnej formy pod&#x13E;a objemu, riz&#xED;k a pl&#xE1;nov rastu.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mistakes-item__item">
+                                    <div class="mistakes-item-item__tilte">
+                                        <h4>Nespr&#xE1;vna vo&#x13E;ba pr&#xE1;vnej formy</h4>
+                                    </div>
+                                    <div class="mistakes-item-item__body">
+                                        <div class="mistakes-item-item__column red">
+                                            <p><strong>Chyba:</strong></p>
+                                            <p>Vo&#x13E;ba medzi &#x17E;ivnos&#x165;ou a s.r.o. bez zoh&#x13E;adnenia &#x161;pecif&#xED;k podnikania.</p>
+                                        </div>
+                                        <div class="mistakes-item-item__column blue">
+                                            <p><strong>Rie&#x161;enie:</strong></p>
+                                            <p>Konzult&#xE1;cia s pr&#xE1;vnikom na v&#xFD;ber optim&#xE1;lnej formy pod&#x13E;a objemu, riz&#xED;k a pl&#xE1;nov rastu.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mistakes__item" id="tax">
+                            <div class="mistakes-item__title">
+                                <h3>Dokument&#xE1;cia a zmluvy</h3>
+                            </div>
+                            <div class="mistakes-item__body">
+                                <div class="mistakes-item__item">
+                                    <div class="mistakes-item-item__tilte">
+                                        <h4>Nespr&#xE1;vna vo&#x13E;ba pr&#xE1;vnej formy</h4>
+                                    </div>
+                                    <div class="mistakes-item-item__body">
+                                        <div class="mistakes-item-item__column red">
+                                            <p><strong>Chyba:</strong></p>
+                                            <p>Vo&#x13E;ba medzi &#x17E;ivnos&#x165;ou a s.r.o. bez zoh&#x13E;adnenia &#x161;pecif&#xED;k podnikania.</p>
+                                        </div>
+                                        <div class="mistakes-item-item__column blue">
+                                            <p><strong>Rie&#x161;enie:</strong></p>
+                                            <p>Konzult&#xE1;cia s pr&#xE1;vnikom na v&#xFD;ber optim&#xE1;lnej formy pod&#x13E;a objemu, riz&#xED;k a pl&#xE1;nov rastu.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul class="technical__list">
+                    <li class="technical-list__item">
+                        <a href="#legal">Pr&#xE1;vne a registra&#x10D;n&#xE9; chyby</a>
+                    </li>
+                    <li class="technical-list__item">
+                        <a href="#tax">Da&#x148;ov&#xE9; a finan&#x10D;n&#xE9; chyby</a>
+                    </li>
+
+                    <div class="technical-list__button button">
+                        <a href>Chcem sa zapoji&#x165;</a>
+                    </div>
+                </ul>
+            </div>
+            <div class="single-head__banner">
+                <p>&#xDA;spe&#x161;n&#xE9; spustenie podnikania na Slovensku si vy&#x17E;aduje komplexn&#xFD; pr&#xED;stup a hlbok&#xE9; porozumenie miestnym &#x161;pecifik&#xE1;m.&#xFEFF; Vyhnutie sa t&#xFD;mto chyb&#xE1;m v&#xFD;razne zvy&#x161;uje &#x161;ance na &#xFA;spech.&#xFEFF; Pam&#xE4;tajte: lep&#x161;ie je probl&#xE9;mom predch&#xE1;dza&#x165;, ne&#x17E; ich potom rie&#x161;i&#x165;.&#xFEFF; Profesion&#xE1;lna podpora vo f&#xE1;ze pl&#xE1;novania a &#x161;tartu sa v bud&#xFA;cnosti mnohon&#xE1;sobne vr&#xE1;ti.&#xFEFF; Slovensk&#xE9; spolo&#x10D;nosti, ktor&#xE9; dosiahli glob&#xE1;lny &#xFA;spech, ako ESET a Sygic, dokazuj&#xFA;, &#x17E;e pri spr&#xE1;vnom pr&#xED;stupe mo&#x17E;no vybudova&#x165; konkurencieschopn&#xFD; biznis. Pripraven&#xED; na &#xFA;spe&#x161;n&#xFD; &#x161;tart? Z&#xED;skajte profesion&#xE1;lne poradenstvo a podporu od expertov, ktor&#xED; poznaj&#xFA; slovensk&#xFD; trh zvn&#xFA;tra.</p>
+            </div>
+
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
